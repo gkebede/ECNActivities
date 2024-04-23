33f1b793f5bc5d53c0fa02a1541f0934233c2092
gkebede <33853640+gkebede@users.noreply.github.com>
2024-04-23 14:13:09 -0400

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+                                                                                                                    
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.   ~~~ OR MIDDLEWARE
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseAuthorization();
+app.MapControllers();
+
+// app.UseHttpsRedirection();
+
+//*** added codes to migrate and seed data to the necessary tables
+using var scope = app.Services.CreateScope();  //the whole injected services are availabel here 
+
+var servises = scope.ServiceProvider;  //from whole injected services pick desire service
+
+try
+{   
+    var context = servises.GetRequiredService<DataContext>(); // pick the right servie(DataContext) to perform the task
+    // var userManager = servises.GetRequiredService<UserManager<AppUser>>();
+      await context.Database.MigrateAsync();
+     await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+    
+    var logger = servises.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurd during migration!");
+}
+
+
+app.Run();
+
+ 
